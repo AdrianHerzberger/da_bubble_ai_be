7164d1df6a502935856b4dfd4ca73b81c330ce66
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-11-15 14:22:14 +0100

    remove string length leftover

--- app/utils/summarization_provider.py
+++ app/utils/summarization_provider.py
@@ -8,10 +8,12 @@ class Summarization:
         return cls._instance
 
     def filter_summarization(self, keywords):
+        unique_keywords = set(self.filtered_keywords)
         for kw in keywords:
-            if kw.strip():
-                self.filtered_keywords.append(kw.strip())
-        unique_keywords = list(set(self.filtered_keywords))
+            if kw.strip() and len(kw) > int(1):
+                print(kw)
+                unique_keywords.add(kw.strip())
+        self.filtered_keywords = list(unique_keywords)
         return self.filtered_keywords
 
     def summarization_result(self):
