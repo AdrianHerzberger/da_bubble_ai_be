97c758c027893de3b649d5f408d8b8ef1d58d199
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-12-13 10:34:32 +0100

    test user auth routes

--- tests/test_user_auth_routes.py
+++ tests/test_user_auth_routes.py
@@ -1,10 +1,10 @@
+from datetime import datetime
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, AsyncMock
 from flask import Flask
 from app.routes.auth_routes import auth_routes
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token
 from config import Config
-
 
 class TestUserAuth(unittest.TestCase):
 
@@ -20,72 +20,73 @@ class TestUserAuth(unittest.TestCase):
         cls.client = cls.app.test_client()
 
 
-    @patch("app.routes.auth_routes.UserDataManager.get_user_by_email")
+    @patch("app.routes.auth_routes.UserDataManager.get_user_by_email", new_callable=AsyncMock)
     @patch("app.routes.auth_routes.UserDataManager.check_user_password")
-    @patch("app.routes.auth_routes.create_access_token")
-    @patch("app.routes.auth_routes.create_refresh_token")
-    async def test_valid_login(self, mock_create_refresh_token, mock_create_access_token, mock_check_user_password, mock_get_user_by_email):
-        mock_get_user_by_email.return_value = MagicMock(
+    @patch("app.routes.auth_routes.UserDataManager.update_user_last_login_date", new_callable=AsyncMock)
+    def test_valid_login(self, mock_update_user_last_login_date, mock_check_user_password, mock_get_user_by_email):
+        with self.app.app_context():
+            access_token = create_access_token(identity="test_user")
+
+        last_login_date = datetime.now()
+
+        mock_get_user_by_email.return_value = AsyncMock(
             id="c62fed14-8e82-44bb-9a74-361115f6e2d6",
             user_name="Test User",
             user_password="hashed_password"
         )
-        mock_check_user_password.return_value = True
-        mock_create_access_token.return_value = "access_token_123"
-        mock_create_refresh_token.return_value = "refresh_token_123"
 
+        mock_check_user_password.return_value = True
+        mock_update_user_last_login_date.return_value = last_login_date
+
+        headers = {"Authorization": f"Bearer {access_token}"} 
         payload = {
             "user_email": "test@example.com",
-            "user_password": "validPassword123"
+            "user_password": "validPassword123!"
         }
 
-        response = await self.client.post("/login", json=payload)
-
-        self.assertEqual(response.status_code, 201)
-        self.assertIn("access_token_123", response.get_data(as_text=True))
-        self.assertIn("refresh_token_123", response.get_data(as_text=True))
+        response = self.client.post("/login", headers=headers, json=payload)
+        response_data = response.get_json()
+        self.assertIn("access_token", response_data)
+        self.assertIn("refresh_token", response_data)
+        self.assertIn("user_id", response_data)
+        self.assertIn("user_name", response_data)
+        self.assertEqual(response_data["message"], "Login successful")
 
     
-    @patch("app.routes.auth_routes.UserDataManager.get_user_by_email")
+    @patch("app.routes.auth_routes.UserDataManager.get_user_by_email", new_callable=AsyncMock)
     @patch("app.routes.auth_routes.UserDataManager.check_user_password")
-    async def test_invalid_password_login(self, mock_check_user_password, mock_get_user_by_email):
-        mock_get_user_by_email.return_value = MagicMock(
+    @patch("app.routes.auth_routes.UserDataManager.update_user_last_login_date", new_callable=AsyncMock)
+    def test_invalid_password_login(self, mock_update_user_last_login_date, mock_check_user_password, mock_get_user_by_email):
+        with self.app.app_context():
+            access_token = create_access_token(identity="test_user")
+
+        last_login_date = datetime.now()
+
+        mock_get_user_by_email.return_value = AsyncMock(
             id="c62fed14-8e82-44bb-9a74-361115f6e2d6",
             user_name="Test User",
             user_password="hashed_password"
         )
 
         mock_check_user_password.return_value = False
+        mock_update_user_last_login_date.return_value = last_login_date
 
+        headers = {"Authorization": f"Bearer {access_token}"} 
         payload = {
             "user_email": "test@example.com",
             "user_password": "wrongPassword123!"
         }
 
-        response = await self.client.post("/login", json=payload)
+        response = self.client.post("/login", headers=headers, json=payload)
+        response_data = response.get_json()
         self.assertEqual(response.status_code, 401)
-        self.assertIn("Failed to login user", response.get_data(as_text=True))
-
-
-    @patch("app.routes.auth_routes.UserDataManager.get_user_by_email")
-    async def test_error_handling(self, mock_get_user_by_email):
-        mock_get_user_by_email.side_effect = Exception("Database connection error")
-
-        payload = {
-            "user_email": "test@example.com",
-            "user_password": "testPassword123"
-        }
-
-        response = await self.client.post("/login", json=payload)
-        self.assertEqual(response.status_code, 500)
-        self.assertIn("Failed to login user", response.get_data(as_text=True))
+        self.assertEqual(response_data["error"], "Invalid email or password")
 
 
     @patch("app.routes.auth_routes.unset_jwt_cookies")
-    async def test_logout(self, mock_unset_jwt_cookies):
+    def test_logout(self, mock_unset_jwt_cookies):
         mock_unset_jwt_cookies.side_effect = lambda response: response
-
-        response = await self.client.post("/api/logout")
+        response = self.client.post("/logout")
 
         self.assertEqual(response.status_code, 200) 
         response_data = response.get_json()
@@ -93,6 +94,9 @@ class TestUserAuth(unittest.TestCase):
         self.assertEqual(response_data["message"], "Logout successful")
 
         mock_unset_jwt_cookies.assert_called_once()
+
+
+
 
 
 
