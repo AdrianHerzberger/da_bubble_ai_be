db44415e9c0bbb187120f0c7c0e573371d90dc6d
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-12-16 16:09:40 +0100

    port comfig

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 import uvicorn
 from app.main import asgi_app
 
 if __name__ == "__main__":
-    #uvicorn.run("app.main:asgi_app", host="127.0.0.1", port=5001, reload=True)
-    uvicorn.run("app.main:asgi_app", host="0.0.0.0", port=5000)
+    #uvicorn.run("app.main:asgi_app", host="127.0.0.1", port=5000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:asgi_app", host="0.0.0.0", port=port)
 
